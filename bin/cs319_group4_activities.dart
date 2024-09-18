3a3f56239baf7e569313dc97ddd6c44c051f4b88
Danne Uriel Boiser <danne.boiser@ciit.edu.ph>
2024-09-18 23:46:44 +0800

    Deleted unnecessary files

--- bin/cs319_group4_activities.dart
+++
@@ -1,5 +0,0 @@
-import 'package:cs319_group4_activities/cs319_group4_activities.dart' as cs319_group4_activities;
-
-void main(List<String> arguments) {
-  print('Hello world: ${cs319_group4_activities.calculate()}!');
-}
--- test/cs319_group4_activities_test.dart
+++
@@ -1,8 +0,0 @@
-import 'package:cs319_group4_activities/cs319_group4_activities.dart';
-import 'package:test/test.dart';
-
-void main() {
-  test('calculate', () {
-    expect(calculate(), 42);
-  });
-}
