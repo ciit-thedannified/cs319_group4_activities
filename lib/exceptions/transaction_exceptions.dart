5e4f99a562ee3368f013db2033e9d6e6b95f8bbb
Danne Uriel Boiser <danne.boiser@ciit.edu.ph>
2024-09-18 23:35:10 +0800

    Added transaction exceptions.

---
+++ lib/exceptions/transaction_exceptions.dart
@@ -0,0 +1,50 @@
+library exceptions;
+
+import 'package:cs319_assignment_activity1/utils/transactions_utils.dart';
+export 'package:cs319_assignment_activity1/exceptions/transaction_exceptions.dart';
+
+class IncorrectTransactionException implements Exception {
+  final TransactionTypes transactionType;
+  String? message;
+
+  IncorrectTransactionException(this.transactionType);
+  IncorrectTransactionException.withMessage(this.transactionType, {String? message});
+
+  @override
+  String toString() {
+    return "IncorrectTransactionException: ${message ?? "$transactionType is not a debit transaction."}";
+  }
+}
+
+class OutOfBalanceException implements Exception {
+  final String message;
+
+  OutOfBalanceException(this.message);
+
+  @override
+  String toString() {
+    return "OutOfBalanceException: $message";
+  }
+}
+
+class IncorrectPinCodeException implements Exception {
+  final String message;
+
+  IncorrectPinCodeException([this.message = "You entered an incorrect PIN code."]);
+
+  @override
+  String toString() {
+    return "IncorrectPinCodeException: $message";
+  }
+}
+
+class NoSuchUserException implements Exception {
+  final String message;
+
+  NoSuchUserException(this.message);
+
+  @override
+  String toString() {
+    return "NoSuchUserException: $message";
+  }
+}
