6cfdfea3b7e2e1e90ac1c21ebcbb8aa689477a89
Danne Uriel Boiser <danne.boiser@ciit.edu.ph>
2024-09-18 23:19:38 +0800

    Initial commit.

---
+++ test/cs319_group4_activities_test.dart
@@ -0,0 +1,8 @@
+import 'package:cs319_group4_activities/cs319_group4_activities.dart';
+import 'package:test/test.dart';
+
+void main() {
+  test('calculate', () {
+    expect(calculate(), 42);
+  });
+}
